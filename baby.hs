d757b344840cdca4d02da1ca1e9626b9de839d86
Vignesh Natarajan <vignesh.natarajan92@gmail.com>
2014-05-01 01:51:03 +0530

    wrote one more function

--- baby.hs
+++ baby.hs
@@ -3,3 +3,9 @@ doubleMe x = x*2
 doubleUs x y = x*2 + y*2
 
 doubleUsUsingdoubleMe x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x y = if x>y then doubleMe y else	doubleMe x
+
+
+--can call functions from other functions only if they're defined withiin the same script 
+	-- either that or I haven't yet figured out how :(
